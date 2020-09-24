603e64ca13b1c6d4575425dd40c01c0337464da7
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2020-09-24 12:05:33 -0300

    fix: Add UUID extension to Postgres if needed

--- lessons/backend/src/database/migrations/1600946538899-CreateAppointments.ts
+++ lessons/backend/src/database/migrations/1600946538899-CreateAppointments.ts
@@ -3,6 +3,8 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 export default class CreateAppointments1600946538899
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+
     await queryRunner.createTable(
       new Table({
         name: 'appointments',
