53869dc7c4b313ad3685a02752cf7d8374dc0ed7
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2020-09-24 11:54:02 -0300

    fix: Appointment UUID generation

--- lessons/backend/src/database/migrations/1600946538899-CreateAppointments.ts
+++ lessons/backend/src/database/migrations/1600946538899-CreateAppointments.ts
@@ -11,7 +11,8 @@ export default class CreateAppointments1600946538899
             name: 'id',
             type: 'varchar',
             isPrimary: true,
-            generationStrategy: 'uuid'
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()'
           },
           {
             name: 'provider',
